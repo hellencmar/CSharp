2f5e481544ccbf2b751f3aef6ac8bdf43014ef09
Hellen Martins <hellencarolinecbj@hotmail.com>
2018-05-10 10:34:05 -0300

    Migrando as informações de Cliente para Pessoa

--- Aula/Aula1005_POO/Cliente.cs
+++ Aula/Aula1005_POO/Cliente.cs
@@ -1,40 +1,9 @@
 ﻿namespace Aula1005_POO
 {
     class Cliente
-    {
-        //Atributo
-        private string nome;        
+    {   
 
-        //Propriedade
-        public string Nome
-        {
-            get
-            {
-                return nome;
-            }
-
-            set
-            {
-                nome = value;
-            }
-        }
-
-        public string Cpf { get; set;}
-
-        //Construtor 
-        public Cliente() //Construtor vazio
-        {
-
-        }
-        public Cliente(string nome)
-        {
-            Nome = nome;
-        }
-        public Cliente(string nome, string cpf)
-        {
-            Nome = nome;
-            Cpf  = cpf;
-        }
+        
 
     }
 }
--- Aula/Aula1005_POO/Pessoa.cs
+++ Aula/Aula1005_POO/Pessoa.cs
@@ -2,6 +2,38 @@
 {
     class Pessoa
     {
+        //Atributo
+        private string nome;
 
+        //Propriedade
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+
+            set
+            {
+                nome = value;
+            }
+        }
+
+        public string Cpf { get; set; }
+
+        //Construtor 
+        public Pessoa() //Construtor vazio
+        {
+
+        }
+        public Pessoa(string nome)
+        {
+            Nome = nome;
+        }
+        public Pessoa(string nome, string cpf)
+        {
+            Nome = nome;
+            Cpf = cpf;
+        }
     }
 }
