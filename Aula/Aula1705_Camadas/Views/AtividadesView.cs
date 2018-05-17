805b4e103ab2ce7ccb1241060c889261e9d1e106
a <@outlook.com>
2018-05-17 09:24:55 -0300

    Criando classe AtividadesView

---
+++ Aula/Aula1705_Camadas/Views/AtividadesView.cs
@@ -0,0 +1,34 @@
+﻿using Aula1705_Camadas.Controllers;
+using Aula1705_Camadas.Models;
+using System;
+
+namespace Aula1705_Camadas.Views
+{
+    class AtividadesView
+    {
+        public int ExibirMenu()
+        {
+            Console.WriteLine("========================");
+            Console.WriteLine("=  Escolha uma opcao:  =");
+            Console.WriteLine("= 1) Criar Atividade   =");
+            Console.WriteLine("= 2) Listar Atividades =");
+            Console.WriteLine("= 3) Buscar Atividade  =");
+            Console.WriteLine("= 4) Editar Atividade  =");
+            Console.WriteLine("= 5) Excluir Atividade =");
+            Console.WriteLine("========================");
+            int opcao = int.Parse(Console.ReadLine());
+            return opcao;
+        }
+
+        public void CriarAtividade()
+        {
+            Atividade atividade = new Atividade();
+            Console.Write("Digite o nome da atividade: ");
+            atividade.Nome = Console.ReadLine();
+            atividade.Ativo = true;
+
+            AtividadesController atividadeCtrl = new AtividadesController();
+            atividadeCtrl.Salvar(atividade);
+        }
+    }
+}
