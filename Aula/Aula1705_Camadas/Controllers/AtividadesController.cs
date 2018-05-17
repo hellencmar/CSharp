070f5c73ce6df363169552f8e53a28ea4d27c24a
a <@outlook.com>
2018-05-17 08:46:53 -0300

    Adicionando using para List e Atividade

--- Aula/Aula1705_Camadas/Controllers/AtividadesController.cs
+++ Aula/Aula1705_Camadas/Controllers/AtividadesController.cs
@@ -1,11 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using Aula1705_Camadas.Models;
+using System.Collections.Generic;
 
 namespace Aula1705_Camadas.Controllers
 {
     class AtividadesController
     {
         //simular a tabela
-        public List<Atividade>  ListaAtividades { get; set; }
+        public List<Atividade>  ListaAtividades { get; set; 
+
         //Salvar
         //Listar
         //Editar
