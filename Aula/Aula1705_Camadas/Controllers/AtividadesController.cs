588fb5c1650a008dbccb5fed37866e78cf303227
a <@outlook.com>
2018-05-17 08:44:27 -0300

    Criando a classe AtividadesController

---
+++ Aula/Aula1705_Camadas/Controllers/AtividadesController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Aula1705_Camadas.Controllers
+{
+    class AtividadesController
+    {
+        //simular a tabela
+        public List<Atividade>  ListaAtividades { get; set; }
+        //Salvar
+        //Listar
+        //Editar
+        //Excluir
+    }
+}
