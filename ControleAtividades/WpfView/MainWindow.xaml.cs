c89b5b0f57de7f30ad76f23c70018bb2892471ed
a <@outlook.com>
2018-06-04 11:22:23 -0300

    Aprendendo as telas

--- ControleAtividades/WpfView/MainWindow.xaml.cs
+++ ControleAtividades/WpfView/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfView
 {
@@ -23,6 +11,23 @@ namespace WpfView
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+
+        private void btnSalvar_Click(object sender, RoutedEventArgs e)
+        {
+            btnSalvar.Content = "aaaa";
+        }
+
+        private void checkBox_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBox chk = (CheckBox)sender;
+            if(chk.IsChecked.HasValue && chk.IsChecked.Value == true)
+            {
+                btnSalvar.Visibility = Visibility.Hidden;
+            }
+            else
+                btnSalvar.Visibility = Visibility.Visible;
         }
     }
 }
